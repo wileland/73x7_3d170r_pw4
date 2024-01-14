b0c9f81001e21be4666129454bbaca7cdc8e2257
wileland <wileland7@gmail.com>
2024-01-14 03:12:11 -0600

    Implement putDb and getDb methods in database.js and update package.json

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,22 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async () => {
+  const db = await initdb();
+  const transaction = db.transaction('jate', 'readwrite');
+  const store = transaction.objectStore('jate');
+  await store.add(content);
+  console.log('Content added to the database');
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+initdb();
+
+export const getDb = async () => {
+  const db = await initdb();
+  const transaction = db.transaction('jate', 'readonly');
+  const store = transaction.objectStore('jate');
+  const content = await store.getAll();
+  return content;
+};
 
 initdb();
--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the 'client' directory
-app.use(express.static("client")); // Adjust the path if your 'client' directory is structured differently
+// Serve static files from the 'client/dist' directory (update if using a build process)
+app.use(express.static(path.join(__dirname, "client")));
 
 // Middleware for parsing application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
@@ -12,8 +13,13 @@ app.use(express.urlencoded({ extended: true }));
 // Middleware for parsing application/json
 app.use(express.json());
 
-// Require the routes in your application, make sure the path is correct
-require("./routes/htmlRoutes")(app);
+// Setup API routes here if you have any
+// app.use('/api', apiRoutes);
+
+// Serve the index.html file for all other requests to support SPA
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "index.html"));
+});
 
 // Start the server on the defined PORT
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
